e76c3922aeebe34c343e158c4dcca9471773d04a
untoldextacy <vanlarec1991@gmail.com>
2024-10-31 19:43:10 -0400

    Client Side Running

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-3xl font-bold text-blue-600">Travel Itinerary Planner</h1>
+      <p className="text-gray-700">Plan your trips efficiently and easily!</p>
+    </div>
+  );
+}
+
+export default App;
